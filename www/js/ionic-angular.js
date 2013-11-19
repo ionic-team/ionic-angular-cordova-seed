637c6236f6e62804dafd01f22b15f0ebd0a3b4b0
Max Lynch <max@drifty.com>
2013-11-18 18:56:25 -0600

    Added nice icons

--- www/js/ionic-angular.js
+++ www/js/ionic-angular.js
@@ -23942,7 +23942,6 @@ angular.module('ionic.ui', [
                             'ionic.ui.content',
                             'ionic.ui.tabs',
                             'ionic.ui.nav',
-                            'ionic.ui.navRouter',
                             'ionic.ui.header',
                             'ionic.ui.sideMenu',
                             'ionic.ui.slideBox',
@@ -23958,7 +23957,6 @@ angular.module('ionic', [
     
     // Angular deps
     'ngAnimate',
-    'ngRoute',
     'ngTouch',
     'ngSanitize'
 ]);
@@ -25258,142 +25256,6 @@ angular.module('ionic.ui.nav', ['ionic.service.templateLoad', 'ionic.service.ges
 
 })();
 ;
-(function() {
-'use strict';
-
-/**
- * @description
- * The NavController is a navigation stack View Controller modelled off of 
- * UINavigationController from Cocoa Touch. With the Nav Controller, you can
- * "push" new "pages" on to the navigation stack, and then pop them off to go
- * back. The NavController controls a navigation bar with a back button and title
- * which updates as the pages switch.
- *
- * The NavController makes sure to not recycle scopes of old pages
- * so that a pop will still show the same state that the user left.
- *
- * However, once a page is popped, its scope is destroyed and will have to be
- * recreated then next time it is pushed.
- *
- */
-
-var actualLocation = null;
-
-angular.module('ionic.ui.navRouter', [])
-
-.run(['$rootScope', function($rootScope) {
-  $rootScope.stackCursorPosition = 0;
-}])
-
-.directive('navRouter', ['$rootScope', '$timeout', '$location', '$window', '$route', function($rootScope, $timeout, $location, $window, $route) {
-  return {
-    restrict: 'AC',
-    link: function($scope, $element, $attr) {
-      $scope.animation = $attr.animation;
-
-      var isFirst = true;
-
-      var reverseTransition = function() {
-        console.log('REVERSE');
-        $element.removeClass($scope.animation);
-        $element.addClass($scope.animation + '-reverse');
-      };
-
-      var forwardTransition = function() {
-        console.log('FORWARD');
-        $element.removeClass($scope.animation + '-reverse');
-        $element.addClass($scope.animation);
-      };
-
-      $scope.$on('$routeChangeSuccess', function(e, a) {
-        console.log('ROUTE CHANGED', a, e);
-      });
-      $scope.$on('$routeChangeStart', function(e, a) {
-        console.log('ROUTE START', a, e);
-        var back, historyState = $window.history.state;
-
-        back = !!(historyState && historyState.position <= $rootScope.stackCursorPosition);
-
-        if(isFirst) {
-          // Don't animate
-          //return;
-        }
-
-        if(back) {
-          reverseTransition();
-        } else {
-          forwardTransition();
-        }
-      });
-
-      $scope.$on('$locationChangeSuccess', function() {
-        // Store the new location
-        console.log('LOCATION CHANGE SUCCESS');
-        $rootScope.actualLocation = $location.path();
-        if(isFirst) {
-          isFirst = false;
-          $timeout(function() {
-            //reverseTransition();
-          }, 200);
-        }
-      });  
-
-
-      // Keep track of location changes and update a stack pointer that tracks whether we are
-      // going forwards or back
-      $scope.$watch(function () { return $location.path() }, function (newLocation, oldLocation) {
-        if($rootScope.actualLocation === newLocation) {
-
-          var back, historyState = $window.history.state;
-
-          back = !!(historyState && historyState.position <= $rootScope.stackCursorPosition);
-
-          if (back) {
-            //back button
-            $rootScope.stackCursorPosition--;
-          } else {
-            //forward button
-            $rootScope.stackCursorPosition++;
-          }
-
-        } else {
-          var currentRouteBeforeChange = $route.current;
-
-          if (currentRouteBeforeChange) {
-
-            $window.history.replaceState({
-              position: $rootScope.stackCursorPosition
-            });
-
-            $rootScope.stackCursorPosition++;
-          }
-        }
-      });
-    }
-  }
-}])
-
-.directive('navBack', ['$window', function($window) {
-  return {
-    restrict: 'AC',
-    require: '^?navRouter',
-    link: function($scope, $element, $attr, navCtrl) {
-      var goBack = function() {
-        $window.history.back();
-      };
-      $element.bind('tap', goBack);
-      $element.bind('click', goBack);
-
-      $scope.$on('$destroy', function() {
-        $element.unbind('tap', goBack);
-        $element.unbind('click', goBack);
-      });
-    }
-  }
-}]);
-
-})();
-;
 (function(ionic) {
 'use strict';
 
@@ -25749,10 +25611,23 @@ angular.module('ionic.ui.tabs', ['ngAnimate'])
     template: '<div class="content"><tab-controller-bar></tab-controller-bar></div>',
     compile: function(element, attr, transclude, tabsCtrl) {
       return function($scope, $element, $attr) {
+        var tabs = $element[0].querySelector('.tabs');
 
         $scope.tabsType = $attr.tabsType || 'tabs-positive';
         $scope.tabsStyle = $attr.tabsStyle;
         $scope.animation = $attr.animation;
+
+        $attr.$observe('tabsStyle', function(val) {
+          if(tabs) {
+            angular.element(tabs).addClass($attr.tabsStyle);
+          }
+        });
+
+        $attr.$observe('tabsType', function(val) {
+          if(tabs) {
+            angular.element(tabs).addClass($attr.tabsType);
+          }
+        });
 
         $scope.$watch('activeAnimation', function(value) {
           //$element.removeClass($scope.animation + ' ' + $scope.animation + '-reverse');
@@ -25821,7 +25696,7 @@ angular.module('ionic.ui.tabs', ['ngAnimate'])
     template: '<div class="tabs">' + 
       '<tab-controller-item title="{{controller.title}}" icon="{{controller.icon}}" icon-on="{{controller.iconOn}}" icon-off="{{controller.iconOff}}" active="controller.isVisible" index="$index" ng-repeat="controller in controllers"></tab-controller-item>' + 
     '</div>',
-    link: function($scope, $element, $attr) {
+    link: function($scope, $element, $attr, tabsCtrl) {
       $element.addClass($scope.tabsType);
       $element.addClass($scope.tabsStyle);
     }
