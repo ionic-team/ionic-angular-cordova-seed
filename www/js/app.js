bbd184c41d621d6b07181e5bf83561537fffd186
Adam Bradley <adamdbradley@gmail.com>
2014-01-14 21:56:38 -0600

    v0.9.20

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-// Ionic Starter App, v0.9.20-alpha
+// Ionic Starter App, v0.9.20
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
